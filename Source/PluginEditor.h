502414fb71cca8319ecb1cdc56a7afc7cb86416c
markusthegeek <markusthegeek@yahoo.com>
2023-02-14 14:21:35 -0800

    implemented basic comp band controls

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -35,22 +35,22 @@ struct RotarySliderWithLabels : juce::Slider
 {
 public:
     RotarySliderWithLabels(
-        juce::RangedAudioParameter& rap, 
+        juce::RangedAudioParameter* rap, 
         const juce::String& unitSuffix,
         const juce::String& title) :
         juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
             juce::Slider::TextEntryBoxPosition::NoTextBox),
-        param(&rap),
+        param(rap),
         suffix(unitSuffix)
     {
         setName(title);
-        setLookAndFeel(&lnf);
+        //setLookAndFeel(&lnf);
     }
 
-    ~RotarySliderWithLabels()
+   /* ~RotarySliderWithLabels()
     {
         setLookAndFeel(nullptr);
-    }
+    }*/
 
     struct LabelPos
     {
@@ -63,12 +63,21 @@ public:
     void paint(juce::Graphics& g) override;
     juce::Rectangle<int> getSliderBounds() const;
     int getTextHeight() const { return 14; }
-    juce::String getDisplayString() const;
-private:
-    LookAndFeel lnf;
-
+    virtual juce::String getDisplayString() const;
+    void changeParam(juce::RangedAudioParameter* p);
+protected:
     juce::RangedAudioParameter* param;
     juce::String suffix;
+};
+//==============================================================================
+struct RatioSlider : RotarySliderWithLabels
+{
+    RatioSlider(
+        juce::RangedAudioParameter* rap,
+        const juce::String& unitSuffix) :
+        RotarySliderWithLabels(rap, unitSuffix, "RATIO") {}
+
+    juce::String getDisplayString() const override;
 };
 //==============================================================================
 struct PowerButton : juce::ToggleButton {};
@@ -113,15 +122,15 @@ struct Placeholder : juce::Component
     juce::Colour customColor;
 };
 //==============================================================================
-//struct RotarySlider : juce::Slider
-//{
-//    RotarySlider() :
-//        juce::Slider(
-//            juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
-//            juce::Slider::TextEntryBoxPosition::NoTextBox
-//        )
-//    {}
-//};
+struct RotarySlider : juce::Slider
+{
+    RotarySlider() :
+        juce::Slider(
+            juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+            juce::Slider::TextEntryBoxPosition::NoTextBox
+        )
+    {}
+};
 //==============================================================================
 template<
     typename Attachment,
@@ -176,7 +185,25 @@ void addLabelPairs(Labels& labels, const ParamType& param, const SuffixType& suf
     labels.add({ 0.f, getValString(param, true, suffix)});
     labels.add({ 1.f, getValString(param, false, suffix)});
 }
+//==============================================================================
+struct CompressorBandControls : juce::Component
+{
+    CompressorBandControls(juce::AudioProcessorValueTreeState& apvts);
+    void resized() override;
+    void paint(juce::Graphics& g) override;
+private:
+    juce::AudioProcessorValueTreeState& apvts;
 
+    RotarySliderWithLabels attackSlider, releaseSlider, thresholdSlider;
+    RatioSlider ratioSlider;
+
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    std::unique_ptr<Attachment>
+        attackSliderAttachment,
+        releaseSliderAttachment,
+        thresholdSliderAttachment,
+        ratioSliderAttachment;
+};
 //==============================================================================
 struct GlobalControls : juce::Component
 {
@@ -210,12 +237,14 @@ public:
     void resized() override;
 
 private:
+    LookAndFeel lnf;
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleMBCompAudioProcessor& audioProcessor;
 
-    Placeholder controlBar, analyzer, /*globalControls,*/ bandControls;
-    GlobalControls globalControls{audioProcessor.apvts};
+    Placeholder controlBar, analyzer /*globalControls,*/ /*bandControls*/;
+    GlobalControls globalControls{ audioProcessor.apvts };
+    CompressorBandControls bandControls{ audioProcessor.apvts };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleMBCompAudioProcessorEditor)
 };
